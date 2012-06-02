e37790294bc4c5727b60332471c6da97fbe34b20
mahmoud albuhisi <msb_2005_m@hotmailcom>
2012-06-02 13:52:52 +0300

    add loop

---
+++ Together/java/mahmood/Loop.java
@@ -0,0 +1,74 @@
+do {
+	switch(num){
+			case 0 :
+			num==0;
+			s=JOptionPane.showMessageDialog(null,0)
+			break;
+		case 1 :
+			num==1;
+			s=JOptionPane.showMessageDialog(null,1)
+			break;
+			case 2 :
+				num==2;
+				s=JOptionPane.showMessageDialog(null,2)
+				break;
+					case 3 :
+			num==3;
+			s=JOptionPane.showMessageDialog(null,3)
+			break;
+				case 4 :
+			num==4;
+			s=JOptionPane.showMessageDialog(null,4)
+			break;
+				case 5 :
+			num==5;
+			s=JOptionPane.showMessageDialog(null,5)
+			break;
+				case 6 :
+			num==6;
+			s=JOptionPane.showMessageDialog(null,6)
+			break;
+				case 7 :
+			num==7;
+		s=JOptionPane.showMessageDialog(null,7)
+			break;
+				case 8 :
+			num==8;
+			s=JOptionPane.showMessageDialog(null,8)
+			break;
+				case 9 :
+			num==9;
+			s=JOptionPane.showMessageDialog(null,9)
+			break;
+				case 10 :
+					if(num==*){
+						
+						s=JOptionPane.showMessageDialog(null,*)
+					}else if(num==**){
+						s=JOptionPane.showMessageDialog(null,+)
+					}
+			
+			break;
+				case 11 :
+			num==#;
+			s=JOptionPane.showMessageDialog(null,#)
+			break;
+	}
+	
+			
+}		
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+				
