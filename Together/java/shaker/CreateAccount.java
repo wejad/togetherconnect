be3829567a74909c97a6767302787f35b156dd25
Ayman Yousf Shaheen <aymn.s@live.com>
2012-06-02 16:11:55 +0300

    new job

--- Together/java/shaker/CreateAccount.java
+++ Together/java/shaker/CreateAccount.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
 
 class CreateAccount{
-	String UserName,s;
-	float Password;
+	static  String UserName,s;
+	static float Password;
 	int count=0;
+	
+	public CreateAccount(String UserName,float Password){
+		this.UserName=UserName;
+		this.Password=Password;
+	}
 	
 	public void Entering(String UserName,float Password){
 	UserName=JOptionPane.showInputDialog("Enter User Name");
@@ -15,20 +20,27 @@ class CreateAccount{
 		String p;
 		UserName=JOptionPane.showInputDialog("Enter The name of User your Create");
 		p=findUser(UserName);
-		if(p==null){
-		
+		if(p==null){		
 	    s=JOptionPane.showInputDialog("Enter The Password");
 	    Password=Float.parseFloat(s);
-	    count++;}
+	    s=JOptionPane.showInputDialog("Enter your Age");
+	    int age = Integer.parseInt(s);
+	    String City = JOptionPane.showInputDialog("Enter your City");
+	    count++;
+	    JOptionPane.showMessageDialog(null,"Creating Account ^_^");
+	    }
 		
 	}
 	
 	public String findUser(String name){
-		name=JOptionPane.showInputDialog("Enter the name");
 		if(UserName==name){
 			return name;
 		}
 		name=null;
 		return name;
+}
+public static void main (String[] args) {
+	CreateAccount a = new CreateAccount(UserName,Password);
+	a.AddUser(UserName,Password);
 }
 }
