3cf2ca5d4b0db7e7ca7b28fae4ca729a9e5c55d7
mahmoud albuhisi <msb_2005_m@hotmailcom>
2012-06-02 13:11:10 +0300

    add loop

--- Together/Loop.java
+++ Together/Loop.java
@@ -2,56 +2,56 @@ do {
 	switch(num){
 			case 0 :
 			num==0;
-			s=JOptionPane.showInternalMessageDialog(null,0);
+			s=JOptionPane.showMessageDialog(null,0)
 			break;
 		case 1 :
 			num==1;
-			s=JOptionPane.showInternalMessageDialog(null,1);
+			s=JOptionPane.showMessageDialog(null,1)
 			break;
 			case 2 :
 				num==2;
-				s=JOptionPane.showInternalMessageDialog(null,2);
+				s=JOptionPane.showMessageDialog(null,2)
 				break;
 					case 3 :
 			num==3;
-			s=JOptionPane.showInternalMessageDialog(null,3);
+			s=JOptionPane.showMessageDialog(null,3)
 			break;
 				case 4 :
 			num==4;
-			s=JOptionPane.showInternalMessageDialog(null,4);
+			s=JOptionPane.showMessageDialog(null,4)
 			break;
 				case 5 :
 			num==5;
-			s=JOptionPane.showInternalMessageDialog(null,5);
+			s=JOptionPane.showMessageDialog(null,5)
 			break;
 				case 6 :
 			num==6;
-			s=JOptionPane.showInternalMessageDialog(null,6);
+			s=JOptionPane.showMessageDialog(null,6)
 			break;
 				case 7 :
 			num==7;
-			s=JOptionPane.showInternalMessageDialog(null,7);
+		s=JOptionPane.showMessageDialog(null,7)
 			break;
 				case 8 :
 			num==8;
-			s=JOptionPane.showInternalMessageDialog(null,8);
+			s=JOptionPane.showMessageDialog(null,8)
 			break;
 				case 9 :
 			num==9;
-			s=JOptionPane.showInternalMessageDialog(null,9);
+			s=JOptionPane.showMessageDialog(null,9)
 			break;
 				case 10 :
 					if(num==*){
 						
-						s=JOptionPane.showInternalMessageDialog(null,*);
+						s=JOptionPane.showMessageDialog(null,*)
 					}else if(num==**){
-						s=JOptionPane.showInternalMessageDialog(null,+);
+						s=JOptionPane.showMessageDialog(null,+)
 					}
 			
 			break;
 				case 11 :
 			num==#;
-			s=JOptionPane.showInternalMessageDialog(null,#);
+			s=JOptionPane.showMessageDialog(null,#)
 			break;
 	}
 	
